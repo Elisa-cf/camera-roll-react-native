f02c730b8c1c7611f0ef04af67820e9fe8de8b7e
Canyelles Elisa <elisa.canyelles@burda-forward.de>
2022-09-14 13:45:15 +0200

    implement camera roll with react native image-picker library

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { FlatList, Image, Button, Text, useWindowDimensions, View, ActivityIndicator } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
 
 export default function App() {
+  const [images, setImages] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const { width } = useWindowDimensions()
+
+  const pickImages = async () => {
+    setIsLoading(true)
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      //allowEditing: true,
+      allowsMultipleSelection: true,
+      selectionLimit: 10,
+      aspect: [4, 3],
+      quality: 1,
+    })
+    setIsLoading(false)
+    console.log(result);
+    if (!result.cancelled) {
+      setImages(result.uri ? [result.uri] : result.selected)
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={images}
+      renderItem={({ item }) => (
+        <Image source={{ uri: item.uri }}
+          style={{ width: width / 3, height: 250, margin: 1 }} />
+      )}
+      numColumns={3}
+      keyExtractor={(item) => item.uri}
+      contentContainerStyle={{ marginVertical: 50, paddingBottom: 100 }}
+      ListHeaderComponent={
+        isLoading ? (
+          <View>
+            <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Loading...</Text>
+            <ActivityIndicator size={"large"} />
+          </View>
+        ) : (
+          <>
+            <Button title="Pick images" onPress={pickImages} />
+          </>
+        )
+      }
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,11 @@
+## create a new project: 
+
+npx create-expo-app name-of-the-app
+
+## install image-picker:
+
+npx expo install expo-image-picker
+
+## to start the app:
+
+npx expo start // npm start + select the devices
